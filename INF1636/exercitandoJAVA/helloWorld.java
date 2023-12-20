bf4848d051285e2d76f0da53a747e5c01eafa007
Thiago Camerato <104781163+thiagocamerato757@users.noreply.github.com>
2023-12-20 02:24:49 -0300

    updated in 20/12/2023

---
+++ INF1636/exercitandoJAVA/helloWorld.java
@@ -0,0 +1,12 @@
+public class helloWorld {// creating a Class with the file's name(always)
+    public static void main(String[] args) { //this class recives various functions 
+        System.out.println("Hello, World!"); //command to print a message
+    }
+}
+
+//compilation of a java file using therminal
+//1 => javac <fileName>.java
+//2 => java <fileName>
+
+//obs: in larger projects it is important to declare packages with the exact folder path
+// ex: package path.to.file
