e93e1408d604f866f543bb69728b6501c0351a71
Thiago Camerato <104781163+thiagocamerato757@users.noreply.github.com>
2024-01-09 13:49:47 -0300

    Update helloWorld.java
    updated in 09/01/2024

--- INF1636/exercitandoJAVA/helloWorld.java
+++ INF1636/exercitandoJAVA/helloWorld.java
@@ -2,6 +2,7 @@ public class helloWorld {// creating a Class with the file's name(always)
     public static void main(String[] args) { //this class recives various functions (always has a Main function)
         System.out.println("Hello, World!"); //command to print a message
     }
+    
 }
 
 //compilation of a java file using therminal
